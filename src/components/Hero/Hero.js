41e3ce959c74fe39948ebf53eb62cd1a816110df
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2022-08-09 23:10:47 +0200

    Add Hero component code

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <div>
+      <h1>Successfully track your tasks!</h1>
+      <p>With help of a simple to-do app, with lists, columns and card</p>
+    </div>
+  );
+};
+
+export default Hero;
