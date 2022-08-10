655f4753366a819fc038d83c8dc2f15c00560a87
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2022-08-10 20:44:30 +0200

    Add code adding new column

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,21 +1,21 @@
 import styles from "./List.module.scss";
 import Column from "../Column/Column";
 import { useState, useEffect } from "react";
+import shortid from 'shortid';
 
 const List = () => {
   const [columns, setColumns] = useState([
-    { id: 1, title: 'Books', icon: 'book' },
-    { id: 2, title: 'Movies', icon: 'film' },
-    { id: 3, title: 'Games', icon: 'gamepad' }
-]);
+    { id: 1, title: "Books", icon: "book" },
+    { id: 2, title: "Movies", icon: "film" },
+    { id: 3, title: "Games", icon: "gamepad" },
+  ]);
 
-useEffect(() => {
-
-  setTimeout(() => {
-      setColumns([...columns, { id: 4, title: 'Test column'}]);
-  }, 2000);
-
-}, []);
+  const [value, setValue] = useState('')
+  const handleSubmit = e => {
+    e.preventDefault();
+    setColumns([...columns, { id: shortid(), title: value }]);
+    setValue('');
+  };
 
   return (
     <div className={styles.list}>
@@ -30,6 +30,10 @@ useEffect(() => {
           <Column key={column.id} title={column.title} icon={column.icon} />
         ))}
       </section>
+      <form onClick={handleSubmit}>
+      <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+        <button>Add column</button>
+      </form>
     </div>
   );
 };
