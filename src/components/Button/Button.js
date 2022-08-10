daec6950dd2df21f250c051122c2cf8aac5c83ab
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2022-08-10 19:02:42 +0200

    Modify Button component with children props

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 import styles from "./Button.module.scss";
 
 const TextInput = props => {
-  return <button className={styles.button}>{props.text}</button>;
+  return <button className={styles.button}>{props.children}</button>;
 };
 
 export default TextInput;
