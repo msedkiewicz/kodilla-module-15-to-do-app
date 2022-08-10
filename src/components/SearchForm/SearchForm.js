ab72a8c44efab3d4cac9106224113cc3dc17c19e
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2022-08-10 16:38:04 +0200

    Add TextInput component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,9 +1,10 @@
 import styles from './SearchForm.module.scss'
+import TextInput from '../TextInput/TextInput'
 
 const SearchForm = () => {
     return (
         <form className={styles.searchForm}>
-            <input className={styles.input} type="text" />
+            <TextInput />
             <button className={styles.button}>Search</button>
         </form>
     );
