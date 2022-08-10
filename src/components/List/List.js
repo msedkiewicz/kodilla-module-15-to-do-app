9de3bcf4ebe7e900bc834d3479a0b2a7e7b1b36f
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2022-08-10 20:21:18 +0200

    Change column creation logic: code with map()

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,17 +1,25 @@
-import styles from './List.module.scss'
-import Column from '../Column/Column'
+import styles from "./List.module.scss";
+import Column from "../Column/Column";
 
 const List = () => {
+  const columns = [
+    { id: 1, title: "Books", icon: "book" },
+    { id: 2, title: "Coding", icon: "code" },
+    { id: 3, title: "Handcfart", icon: "camera" },
+  ];
+
   return (
     <div className={styles.list}>
       <header className={styles.header}>
-        <h2 className={styles.title}>Interesting things to do <span>soon</span>.</h2>
+        <h2 className={styles.title}>
+          Interesting things to do <span>soon</span>.
+        </h2>
       </header>
       <p className={styles.description}>My personal bucket list</p>
       <section className={styles.columns}>
-        <Column title="Books" icon="book" />
-        <Column title="Coding" icon="code" />
-        <Column title="Handcraft" icon="camera" />
+        {columns.map((column) => (
+          <Column key={column.id} title={column.title} icon={column.icon} />
+        ))}
       </section>
     </div>
   );
