8dad578c29c843bfbea2f21c684ea7b699a2a31b
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2022-08-12 18:47:55 +0200

    Add changed Text Input component to ColumnForm

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,6 +1,7 @@
 // import styles from "./ColumnForm.module.scss";
 import { useState } from "react";
-import Button from '../Button/Button'
+import Button from "../Button/Button";
+import TextInput from "../TextInput/TextInput"
 
 const ColumnForm = (props) => {
   const [title, setTitle] = useState("");
@@ -15,17 +16,8 @@ const ColumnForm = (props) => {
   return (
     <form onSubmit={handleSubmit}>
       Title:{" "}
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      Icon:{" "}
-      <input
-        type="text"
-        value={icon}
-        onChange={(e) => setIcon(e.target.value)}
-      />
+      <TextInput value={title} onChange={(e) => setTitle(e.target.value)} />{" "}
+      Icon: <TextInput value={icon} onChange={(e) => setIcon(e.target.value)} />
       <Button>Add column</Button>
     </form>
   );
