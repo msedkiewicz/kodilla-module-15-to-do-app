e21cb8a9fbe5a1c14224d060f4d175faed644f00
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-09-29 18:56:46 +0200

    Add initialState to Redux

---
+++ src/redux/initialState.js
@@ -0,0 +1,30 @@
+const initialState = {
+  columns: [
+    {
+      id: 1,
+      title: "Books",
+      icon: "book",
+    },
+    {
+      id: 2,
+      title: "Programming",
+      icon: "code",
+    },
+    {
+      id: 3,
+      title: "Handcraft",
+      icon: "camera",
+    },
+  ],
+
+  cards: [
+    { id: 1, columnId: 1, title: "Pomnik Cesarzowej Achai" },
+    { id: 2, columnId: 1, title: "Szogun" },
+    { id: 3, columnId: 2, title: "JavaScript" },
+    { id: 4, columnId: 2, title: "Python" },
+    { id: 5, columnId: 3, title: "Skrzynia skarbów" },
+    { id: 6, columnId: 3, title: "Pudełko na kosmetyki do łazienki" },
+  ],
+};
+
+export default initialState;
