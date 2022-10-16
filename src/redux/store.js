c53b66ce839c3300915606ad732c321f693f16c5
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-16 22:14:27 +0200

    Add selector getAllColumns

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,6 +6,7 @@ import strContains from "../utils/strContains";
 //selectors
 export const getFilteredCards = ({ cards, searchString }, columnId) => cards
   .filter(card => card.columnId === columnId && strContains(card.title, searchString));
+export const getAllColumns = (state => state.columns);
 
 const reducer = (state, action) => {
   switch (action.type) {
