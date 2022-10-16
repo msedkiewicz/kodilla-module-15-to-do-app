eb1106f66575cbc218a835f06e4796e334a67105
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-16 09:04:26 +0200

    Add id to new Column

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { createStore } from 'redux';
 import initialState from './initialState';
+import shortid from "shortid";
 
 const reducer = (state, action) => {
-  if(action.type === 'ADD_COLUMN') return { ...state, columns: [...state.columns, action.newColumn]}
+  if(action.type === 'ADD_COLUMN') return { ...state, columns: [...state.columns, {...action.newColumn, id: shortid()}]}
   return state;
 };
 
