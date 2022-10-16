22872ee2e8dcae69d9ec1726066ca34dfdd93f40
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-16 19:47:23 +0200

    Code refactoring

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -2,7 +2,7 @@ import styles from "./ColumnForm.module.scss";
 import { useState } from "react";
 import Button from "../Button/Button";
 import TextInput from "../TextInput/TextInput";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 
 const ColumnForm = (props) => {
   const [title, setTitle] = useState("");
@@ -12,16 +12,23 @@ const ColumnForm = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch({ type: 'ADD_COLUMN', payload: { title, icon } });
+    dispatch({ type: "ADD_COLUMN", payload: { title, icon } });
     setTitle("");
     setIcon("");
   };
   return (
     <form className={styles.columnForm} onSubmit={handleSubmit}>
       Title:
-      <div className={styles.padding}><TextInput value={title} onChange={(e) => setTitle(e.target.value)} /></div>
-      Icon: <div className={styles.padding}><TextInput value={icon} onChange={(e) => setIcon(e.target.value)} /></div>
-      <div className={styles.padding}><Button>Add column</Button></div>
+      <div className={styles.padding}>
+        <TextInput value={title} onChange={(e) => setTitle(e.target.value)} />
+      </div>
+      Icon:{" "}
+      <div className={styles.padding}>
+        <TextInput value={icon} onChange={(e) => setIcon(e.target.value)} />
+      </div>
+      <div className={styles.padding}>
+        <Button>Add column</Button>
+      </div>
     </form>
   );
 };
