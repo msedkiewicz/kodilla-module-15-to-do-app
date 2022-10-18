9be6610114a0278ef033965e6327d748a84ad664
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-18 20:27:03 +0200

    Fix NavBar styling; add missing styles

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,20 +2,28 @@ import styles from "./NavBar.module.scss";
 import Container from "../Container/Container";
 import { Link } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
-
 const NavBar = () => {
-    return (
-      <nav className={styles.navigaton} >
-        <Container>
+  return (
+    <nav className={styles.navbar}>
+      <Container>
         <BrowserRouter>
-            <Link to="/"></Link>
-            <ul className={styles.navigaton}>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/favorite">Favorite</Link></li>
-                <li><Link to="/about">About</Link></li>
+          <div className={styles.navigation}>
+          <i className={styles.icon + " fa fa-tasks"} aria-hidden="true"><Link to="/" /></ i>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/favorite">Favorite</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
             </ul>
+          </div>
         </BrowserRouter>
-        </Container>
-    </nav>);
-}
-    export default NavBar;
+      </Container>
+    </nav>
+  );
+};
+export default NavBar;
