b6887332becd594aa536af8ac504457e5bd6d827
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-18 19:09:13 +0200

    Add NavBar component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import styles from "./NavBar.module.scss";
+import Container from "../Container/Container";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+    return (
+      <nav>
+        <Container>
+            <Link to="/"></Link>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/favorite">Favorite</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
+        </Container>
+    </nav>);
+}
+    export default NavBar;
