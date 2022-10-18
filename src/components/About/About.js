957465d2281780106f238c53db1e62e6417c9eec
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-18 20:47:29 +0200

    Add about component

---
+++ src/components/About/About.js
@@ -0,0 +1,16 @@
+import Container from "../Container/Container";
+import NavBar from "../NavBar/NavBar";
+
+const About = () => {
+  return (
+    <div>
+      <NavBar />
+      <Container>
+        <h2>About</h2>
+        <p>Lorem ipsum</p>
+      </Container>
+    </div>
+  );
+};
+
+export default About;
