a519c327763e184aae31ce5e52933a64c36dacdf
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-18 19:17:34 +0200

    Fix navigation errors

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,20 @@
 import styles from "./NavBar.module.scss";
 import Container from "../Container/Container";
 import { Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const NavBar = () => {
     return (
       <nav>
         <Container>
+        <BrowserRouter>
             <Link to="/"></Link>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/favorite">Favorite</Link></li>
                 <li><Link to="/about">About</Link></li>
             </ul>
+        </BrowserRouter>
         </Container>
     </nav>);
 }
