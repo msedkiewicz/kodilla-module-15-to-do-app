4a3c1172dafa88ce0e553e9b747ef3618f6b26fe
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-18 20:46:12 +0200

    Add Favourite component; fix typo

---
+++ src/components/Favourite/Favourite.js
@@ -0,0 +1,16 @@
+import Container from "../Container/Container";
+import NavBar from "../NavBar/NavBar";
+
+const Favourite = () => {
+  return (
+    <div>
+      <NavBar />
+      <Container>
+        <h2>Favourite</h2>
+        <p>Lorem ipsum</p>
+      </Container>
+    </div>
+  );
+};
+
+export default Favourite;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,10 @@
 import styles from "./NavBar.module.scss";
 import Container from "../Container/Container";
 import { Link } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav className={styles.navbar}>
       <Container>
-        <BrowserRouter>
           <div className={styles.navigation}>
           <i className={styles.icon + " fa fa-tasks"} aria-hidden="true"><Link to="/" /></ i>
             <ul>
@@ -14,14 +12,13 @@ const NavBar = () => {
                 <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to="/favorite">Favorite</Link>
+                <Link to="/favourite">Favourite</Link>
               </li>
               <li>
                 <Link to="/about">About</Link>
               </li>
             </ul>
           </div>
-        </BrowserRouter>
       </Container>
     </nav>
   );
