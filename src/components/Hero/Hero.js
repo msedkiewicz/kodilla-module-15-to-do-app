58d50158d5189a12c544bf5ed0d2ea6a73a8ab92
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-18 21:22:38 +0200

    Fix double navbar error

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,14 +1,19 @@
-import styles from './Hero.module.scss';
+import styles from "./Hero.module.scss";
+import NavBar from "../NavBar/NavBar";
 import PageTitle from "../PageTitle/PageTitle";
 
 const Hero = () => {
   return (
-    <div className={styles.hero}>
-      <PageTitle>My Bucket List</PageTitle>
-      <p className={styles.subtitle}>A simple to-do app, with lists, columns and cards</p>
-    </div>
+    <>
+      <NavBar />
+      <div className={styles.hero}>
+        <PageTitle>My Bucket List</PageTitle>
+        <p className={styles.subtitle}>
+          A simple to-do app, with lists, columns and cards
+        </p>
+      </div>
+    </>
   );
 };
-
 
 export default Hero;
