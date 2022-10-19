498be0ebb470d4bbf4fd2c0a0d94268b5b9d8b71
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-19 21:44:49 +0200

    Add routing to different Lists

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import About from "./components/About/About";
 import Favourite from "./components/Favourite/Favourite";
 import { Routes, Route } from "react-router-dom";
 import NotFound from "./components/NotFound/NotFound";
+import List from "./components/List/List";
 
 const App = () => {
   return (
     <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/list/:listId" element={<List />} />
           <Route path="/about" element={<About />} />
           <Route path="/favourite" element={<Favourite />} />
           <Route path="*" element={<NotFound />} />
