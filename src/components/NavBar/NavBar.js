9373739a1c9341de525c9a57ff3ae1af3399935a
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-19 19:52:10 +0200

    Add NavLink and styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,24 +1,47 @@
 import styles from "./NavBar.module.scss";
 import Container from "../Container/Container";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav className={styles.navbar}>
       <Container>
-          <div className={styles.navigation}>
-          <i className={styles.icon + " fa fa-tasks"} aria-hidden="true"><Link to="/" /></ i>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/favourite">Favourite</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-            </ul>
-          </div>
+        <div className={styles.navigation}>
+          <i className={styles.icon + " fa fa-tasks"} aria-hidden="true">
+            <Link to="/" />
+          </i>
+          <ul>
+            <li>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? styles.linkActive : undefined
+                }
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? styles.linkActive : undefined
+                }
+                to="/favourite"
+              >
+                Favourite
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? styles.linkActive : undefined
+                }
+                to="/about"
+              >
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </Container>
     </nav>
   );
