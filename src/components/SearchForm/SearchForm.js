c3aa1c69ab80cdcddb05b5c056440fecf57f59b1
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-19 23:16:36 +0200

    Clearing search form results

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,16 +1,18 @@
 import styles from "./SearchForm.module.scss";
 import TextInput from "../TextInput/TextInput";
 import Button from "../Button/Button";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useState } from 'react';
-import { updateSearchString } from "../../redux/store";
+import { updateSearchString, searchInputValue } from "../../redux/store";
 
 const SearchForm = (props) => {
+  const currentSearchString = useSelector(searchInputValue);
+  const [searchValue, setSearchValue] = useState(currentSearchString); // why use state?
   const dispatch = useDispatch();
-  const [searchValue, setSearchValue] = useState(''); // why use state?
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(updateSearchString( searchValue ));
+    dispatch(updateSearchString(searchValue));
+    setSearchValue('');
   };
   return (
     <form className={styles.searchForm}  onSubmit={handleSubmit}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -10,14 +10,20 @@ export const getFilteredCards = ({ cards, searchString }, columnId) =>
       card.columnId === columnId && strContains(card.title, searchString)
   );
 export const getAllColumns = (state) => state.columns;
-export const getListById = ({ lists }, listId) => lists.find(list => list.id === listId); // koństrukcja...
-export const getColumnsByList = ({ columns }, listId) => columns.filter((column) => column.listId === listId); // koństrukcja
+export const getListById = ({ lists }, listId) =>
+  lists.find((list) => list.id === listId); // koństrukcja...
+export const getColumnsByList = ({ columns }, listId) =>
+  columns.filter((column) => column.listId === listId); // koństrukcja
 export const getAllLists = (state) => state.lists;
+export const searchInputValue = (state) => state.searchString;
 
 // action creators
 export const addColumn = (payload) => ({ type: "ADD_COLUMN", payload });
 export const addCard = (payload) => ({ type: "ADD_CARD", payload });
-export const updateSearchString = (payload) => ({  type: "UPDATE_SEARCHSTRING", payload });
+export const updateSearchString = (payload) => ({
+  type: "UPDATE_SEARCHSTRING",
+  payload,
+});
 
 //data
 const reducer = (state, action) => {
