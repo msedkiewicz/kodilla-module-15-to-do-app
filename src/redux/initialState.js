d5ee1678e70121331574a7f62cb333ee600841c3
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-21 20:05:09 +0200

    Add isFavourite information to cards

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -45,14 +45,14 @@ const initialState = {
   ],
 
   cards: [
-    { id: '1', columnId: '1', title: "Pomnik Cesarzowej Achai" },
-    { id: '2', columnId: '1', title: "Szogun" },
-    { id: '3', columnId: '2', title: "JavaScript" },
-    { id: '4', columnId: '2', title: "Python" },
-    { id: '5', columnId: '3', title: "Skrzynia skarbów" },
-    { id: '6', columnId: '3', title: "Pudełko na kosmetyki do łazienki" },
-    { id: '7', columnId: '4', title: "Gregorian" },
-    { id: '8', columnId: '4', title: "Nightwish" },
+    { id: '1', columnId: '1', title: "Pomnik Cesarzowej Achai", isFavourite: true},
+    { id: '2', columnId: '1', title: "Szogun", isFavourite: false },
+    { id: '3', columnId: '2', title: "JavaScript", isFavourite: true },
+    { id: '4', columnId: '2', title: "Python", isFavourite: false },
+    { id: '5', columnId: '3', title: "Skrzynia skarbów", isFavourite: false },
+    { id: '6', columnId: '3', title: "Pudełko na kosmetyki do łazienki", isFavourite: false },
+    { id: '7', columnId: '4', title: "Gregorian", isFavourite: false },
+    { id: '8', columnId: '4', title: "Nightwish", isFavourite: false},
   ],
 
   searchString: '', // why here, not separately? Is it because we need to access other data from same state? or it's just a construction to thor everything in one place?
