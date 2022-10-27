43b60d25871bbce236403f8a10c01c82fc368e91
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-27 18:15:53 +0200

    Add interactive search form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,20 +2,20 @@ import styles from "./SearchForm.module.scss";
 import TextInput from "../TextInput/TextInput";
 import Button from "../Button/Button";
 import { useDispatch, useSelector } from "react-redux";
-import { useState } from 'react';
+// import { useState } from 'react';
 import { updateSearchString, searchInputValue } from "../../redux/store";
 
 const SearchForm = (props) => {
   const currentSearchString = useSelector(searchInputValue);
-  const [searchValue, setSearchValue] = useState(currentSearchString);
+  // const [searchValue, setSearchValue] = useState(currentSearchString);
   const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(updateSearchString(searchValue));
+    dispatch(updateSearchString(e.target.value));
   };
   return (
     <form className={styles.searchForm}  onSubmit={handleSubmit}>
-      <TextInput placeholder="Search..." value={searchValue} onChange={e => setSearchValue(e.target.value)} />
+      <TextInput placeholder="Search..." value={currentSearchString} onChange={e => handleSubmit(e)} />
       <Button>
         <span className="fa fa-search" />
       </Button>
