6eb85c0ae4a122c8fa5145f4e89dadaa1ff16992
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-27 17:39:28 +0200

    Fix typo

--- src/redux/store.js
+++ src/redux/store.js
@@ -88,8 +88,8 @@ const cardsReducer = (statePart = [], action) => {
   switch(action.type) {
     case 'ADD_CARD':
       return [...statePart, { ...action.payload, id: shortid() }];
-    case 'TOGGLE_CARD_FAVORITE':
-      return statePart.map(card => (card.id === action.payload) ? { ...card, isFavorite: !card.isFavorite } : card);
+    case 'TOGGLE_CARD_FAVOURITE':
+      return statePart.map(card => (card.id === action.payload) ? { ...card, isFavourite: !card.isFavourite } : card);
     default:
       return statePart;
   }
